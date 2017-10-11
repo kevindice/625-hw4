1fe1247dc8d4fddc908778b1c7c59d060bd6504a
Kevin Dice <kevin.dice1@gmail.com>
2017-10-11 11:32:06 -0500

    Generate test files

---
+++ generate-test-files.sh
@@ -0,0 +1,5 @@
+head -n 100 /homes/dan/625/wiki_dump.txt > test10-2.txt
+head -n 1000 /homes/dan/625/wiki_dump.txt > test10-3.txt
+head -n 10000 /homes/dan/625/wiki_dump.txt > test10-4.txt
+head -n 100000 /homes/dan/625/wiki_dump.txt > test10-5.txt
+ln -s /homes/dan/625/wiki_dump.txt test10-6.txt
---
+++ local-generate-test-files.sh
@@ -0,0 +1,5 @@
+head -n 10 test10-6.txt > test10-1.txt
+head -n 100 test10-6.txt > test10-2.txt
+head -n 1000 test10-6.txt > test10-3.txt
+head -n 10000 test10-6.txt > test10-4.txt
+head -n 100000 test10-6.txt > test10-5.txt
