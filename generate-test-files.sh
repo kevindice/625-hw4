ec4e2b6f1a3123b6dbfb50258eff2eeaab2d36f8
Kevin Dice <kevin.dice1@gmail.com>
2017-10-11 12:25:36 -0500

    Add keyword file copy to generate script

--- generate-test-files.sh
+++ generate-test-files.sh
@@ -3,3 +3,4 @@ head -n 1000 /homes/dan/625/wiki_dump.txt > test10-3.txt
 head -n 10000 /homes/dan/625/wiki_dump.txt > test10-4.txt
 head -n 100000 /homes/dan/625/wiki_dump.txt > test10-5.txt
 ln -s /homes/dan/625/wiki_dump.txt test10-6.txt
+cp /homes/dan/625/keywords.txt .
