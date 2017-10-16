10eff869a18ba6092ab68711a5699c505bf4cd31
Kevin Dice <kevin.dice1@gmail.com>
2017-10-16 15:32:48 -0500

    Add ring example

--- toy-examples/ring.c
+++ toy-examples/ring.c
@@ -1,32 +1,23 @@
 #include <stdio.h>
-#include "mpi.h"
-
-int main( argc, argv )
-int argc;
-char **argv;
+#include <mpi.h>
+int main(int argc, char **argv)
 {
-    int rank, value, size;
-    MPI_Status status;
+  int rank, size, left, tag=99;
+  MPI_Request request;
 
-    MPI_Init( &argc, &argv );
+  MPI_Status status;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &size );
-    do {
-	if (rank == 0) {
-	    scanf( "%d", &value );
-	    MPI_Send( &value, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD );
-	}
-	else {
-	    MPI_Recv( &value, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, 
-		      &status );
-	    if (rank < size - 1) 
-		MPI_Send( &value, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD );
-	}
-	printf( "Process %d got %d\n", rank, value );
-    } while (value >= 0);
+  MPI_Isend(&rank, 1, MPI_INTEGER, (rank+1)%size, tag, MPI_COMM_WORLD, &request);
+  MPI_Recv(&left, 1, MPI_INTEGER, (rank+size-1)%size, tag,
+    MPI_COMM_WORLD, &status);
+  MPI_Wait( &request, &status );
 
-    MPI_Finalize( );
-    return 0;
+  printf("I am MPI process %d of %d, on my left is %d\n", rank, size, left);
+
+  MPI_Finalize();
+
+  return 0;
 }
-
