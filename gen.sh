836227146e96ef02351a515c78f8b42436998ecf
Kevin Dice <kevin.dice1@gmail.com>
2017-10-19 01:18:25 -0500

    Add gensh

---
+++ gen.sh
@@ -0,0 +1,16 @@
+declare -A timemap
+timemap[1]="2:45:00"
+timemap[2]="2:20:00"
+timemap[4]="1:20:00"
+timemap[8]="1:00:00"
+timemap[16]="0:40:00"
+timemap[20]="0:35:00"
+timemap[32]="0:28:00"
+timemap[48]="0:24:00"
+timemap[64]="0:20:00"
+
+for x in 2 4 8 16
+do
+echo "qsub -N 'mpi-fill$x' -pe mpi-fill $x -l h_rt=${timemap[$x]} mpi-run.sh"
+done;
+
